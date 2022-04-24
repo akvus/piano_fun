345726ab6ca6062cc41f248f339690f02559e521
akvus <mr.akvus@gmail.com>
2022-04-24 23:17:22 +0200

    Outline what to do

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_midi_command/flutter_midi_command.dart';
 
+/*
+MidiPacket -> 3 ints
+Example: https://github.com/InvisibleWrench/FlutterMidiCommand/tree/master/example/lib
+
+
+TODO basic
+- Architect the app a little xD
+- Map codes to notes C1->C6 etc.
+- Define chords
+- Try the piano package for UI, or develop own UI to display chords
+- Logic to randomly ask for a chord, match input, display result with clef and virtual keyboard
+- 
+
+TODO if having too much time (like that ever is a case lol)
+- Each test has X chords, need to hit correct the 1st time, display % of correct per test
+- Record tries and draw some simple stats, charts etc. make it fancy
+*/
+
 void main() {
   runApp(const MyApp());
 }
@@ -60,13 +78,18 @@ class _MyHomePageState extends State<MyHomePage> {
           StreamBuilder(
               stream: midiCommand.onMidiDataReceived,
               builder: (context, snapshopt) {
-                final midiPacket = snapshopt.data as MidiPacket?;
+                final packet = snapshopt.data as MidiPacket?;
 
-                if (midiPacket == null) {
-                  return Text('No midi packet');
+                if (packet == null) {
+                  return const Text('No midi packet');
                 }
+                final data = packet.data;
 
-                return Text(midiPacket.data.toString());
+                final status = data[0];
+                final key = data[0];
+                final strength = data[0]; // what is it really called?
+
+                return Text(data.toString());
               }),
         ],
       ),
