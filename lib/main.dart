060342b1e35f2d04e5c2b7bf1fc578a0298f50b7
akvus <mr.akvus@gmail.com>
2022-04-24 22:58:12 +0200

    Test connection to a physical MIDI device and receiving data ->> success

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_midi_command/flutter_midi_command.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,58 +11,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    final MidiCommand midiCommand = MidiCommand();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      appBar: AppBar(title: const Text('Chords test')),
+      body: Column(
+        children: [
+          FutureBuilder(
+            future: midiCommand.devices,
+            builder: (context, snapshot) {
+              final devices = snapshot.data as List<MidiDevice>;
+              return Column(
+                children: devices
+                    .map((e) => Row(
+                          children: [
+                            Text(e.name),
+                            ElevatedButton(
+                              onPressed: () {
+                                midiCommand.connectToDevice(e);
+                              },
+                              child: const Text('Connect'),
+                            ),
+                          ],
+                        ))
+                    .toList(),
+              );
+            },
+          ),
+          const SizedBox(height: 16),
+          StreamBuilder(
+              stream: midiCommand.onMidiDataReceived,
+              builder: (context, snapshopt) {
+                final midiPacket = snapshopt.data as MidiPacket?;
+
+                if (midiPacket == null) {
+                  return Text('No midi packet');
+                }
+
+                return Text(midiPacket.data.toString());
+              }),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
