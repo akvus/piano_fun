ea38d4f10d0e7abf50cf41573e10a9ef201da292
akvus <mr.akvus@gmail.com>
2022-04-25 17:46:23 +0200

    Use midi command as data source

--- lib/feature/chords/chords_test_page_view_model.dart
+++ lib/feature/chords/chords_test_page_view_model.dart
@@ -1,9 +1,17 @@
+import 'package:flutter_midi_command/flutter_midi_command.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:piano_chords_test/feature/chords/data/midi_command.dart';
 
 final chordsTestPageViewModelProvder = StateNotifierProvider(
-  (ref) => ChordsTestPageViewModel(),
+  (ref) => ChordsTestPageViewModel(ref.read(
+    midiCommandProvider,
+  )),
 );
 
 class ChordsTestPageViewModel extends StateNotifier<Object?> {
-  ChordsTestPageViewModel() : super(null);
+  ChordsTestPageViewModel(
+    this._midiCommand,
+  ) : super(null);
+
+  final MidiCommand _midiCommand;
 }
---
+++ lib/feature/chords/data/midi_command.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_midi_command/flutter_midi_command.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final midiCommandProvider = Provider((ref) => MidiCommand());
