1ce8817818ef719e6c2f261a1cc195b225110af4
akvus <mr.akvus@gmail.com>
2022-04-25 22:20:31 +0200

    Remove test page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_midi_command/flutter_midi_command.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:piano_chords_test/feature/chords/view/chords_test_page.dart';
 
@@ -15,66 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.orange),
       home: const ChordsTestPage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    final MidiCommand midiCommand = MidiCommand();
-
-    return Scaffold(
-      appBar: AppBar(title: const Text('Chords test')),
-      body: Column(
-        children: [
-          FutureBuilder(
-            future: midiCommand.devices,
-            builder: (context, snapshot) {
-              final devices = snapshot.data as List<MidiDevice>;
-              return Column(
-                children: devices
-                    .map((e) => Row(
-                          children: [
-                            Text(e.name),
-                            ElevatedButton(
-                              onPressed: () {
-                                midiCommand.connectToDevice(e);
-                              },
-                              child: const Text('Connect'),
-                            ),
-                          ],
-                        ))
-                    .toList(),
-              );
-            },
-          ),
-          const SizedBox(height: 16),
-          StreamBuilder(
-              stream: midiCommand.onMidiDataReceived,
-              builder: (context, snapshopt) {
-                final packet = snapshopt.data as MidiPacket?;
-
-                if (packet == null) {
-                  return const Text('No midi packet');
-                }
-                final data = packet.data;
-
-                final status = data[0];
-                final key = data[0];
-                final strength = data[0]; // what is it really called?
-
-                return Text(data.toString());
-              }),
-        ],
-      ),
     );
   }
 }
