89564e291c48a21a1f3160de2fc263767806b414
akvus <mr.akvus@gmail.com>
2022-04-28 13:50:09 +0200

    Match with alternative accidentals

--- lib/feature/chords/data/chord_repository.dart
+++ lib/feature/chords/data/chord_repository.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:piano_chords_test/feature/chords/data/note_repository.dart';
+import 'package:piano/piano.dart';
 import 'package:piano_chords_test/feature/chords/domain/chord.dart';
 
 final randomizerProvider = Provider((ref) => Random());
@@ -9,56 +9,49 @@ final randomizerProvider = Provider((ref) => Random());
 final chordRepositoryProvider = Provider.autoDispose(
   (ref) => ChordRepository(
     ref.read(randomizerProvider),
-    ref.read(noteRepositoryProvider),
   ),
 );
 
 class ChordRepository {
   ChordRepository(
     this._randomizer,
-    this._noteRepository,
   );
 
   final Random _randomizer;
-  final NoteRepository _noteRepository;
 
   List<Chord>? _cache;
 
   Chord get random => all[_randomizer.nextInt(all.length)];
 
   List<Chord> get all {
-    if (_cache == null) {
-      final notes = _noteRepository.all;
-
-      _cache = [
-        // TODO fill in xDD
-        Chord(
-          name: 'C major',
-          notes: [
-            notes[48]!,
-            notes[52]!,
-            notes[55]!,
-          ],
-        ),
-        Chord(
-          name: 'C minor',
-          notes: [
-            notes[48]!,
-            notes[51]!,
-            notes[55]!,
-          ],
-        ),
-        Chord(
-          name: 'C7',
-          notes: [
-            notes[48]!,
-            notes[52]!,
-            notes[55]!,
-            notes[57]!,
-          ],
-        ),
-      ];
-    }
+    _cache ??= [
+      // TODO fill in xDD
+      Chord(
+        name: 'C major',
+        notes: [
+          NotePosition(note: Note.C, octave: 3),
+          NotePosition(note: Note.E, octave: 3),
+          NotePosition(note: Note.G, octave: 3),
+        ],
+      ),
+      Chord(
+        name: 'C minor',
+        notes: [
+          NotePosition(note: Note.C, octave: 3),
+          NotePosition(note: Note.E, octave: 3, accidental: Accidental.Flat),
+          NotePosition(note: Note.G, octave: 3),
+        ],
+      ),
+      Chord(
+        name: 'C7',
+        notes: [
+          NotePosition(note: Note.C, octave: 3),
+          NotePosition(note: Note.E, octave: 3),
+          NotePosition(note: Note.G, octave: 3),
+          NotePosition(note: Note.B, octave: 3),
+        ],
+      ),
+    ];
 
     return _cache!;
   }
--- lib/feature/chords/domain/match_chord_use_case.dart
+++ lib/feature/chords/domain/match_chord_use_case.dart
@@ -2,8 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:piano/piano.dart';
 import 'package:piano_chords_test/feature/chords/domain/chord.dart';
 
-final matchChordUseCaseProvider =
-    Provider.autoDispose((ref) => MatchChordUseCase());
+final matchChordUseCaseProvider = Provider.autoDispose(
+  (ref) => MatchChordUseCase(),
+);
 
 enum ChordMatch { matched, partial, failed }
 
@@ -12,7 +13,8 @@ class MatchChordUseCase {
     int matchesCount = 0;
 
     for (final note in notes) {
-      if (chord.notes.contains(note)) {
+      if (chord.notes.contains(note) ||
+          chord.notes.contains(note.alternativeAccidental)) {
         matchesCount++;
       } else {
         return ChordMatch.failed;
