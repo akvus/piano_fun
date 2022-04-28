8287f6cee3ad07c34582634e0fba301606389256
akvus <mr.akvus@gmail.com>
2022-04-28 15:03:42 +0200

    Add a couple more cords for testing

--- lib/feature/chords/data/chord_repository.dart
+++ lib/feature/chords/data/chord_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:piano/piano.dart';
 import 'package:piano_chords_test/feature/chords/domain/chord.dart';
@@ -27,7 +28,7 @@ class ChordRepository {
     _cache ??= [
       // TODO fill in xDD
       Chord(
-        name: 'C major',
+        name: 'C',
         notes: [
           NotePosition(note: Note.C, octave: 3),
           NotePosition(note: Note.E, octave: 3),
@@ -35,7 +36,7 @@ class ChordRepository {
         ],
       ),
       Chord(
-        name: 'C minor',
+        name: 'Cm',
         notes: [
           NotePosition(note: Note.C, octave: 3),
           NotePosition(note: Note.E, octave: 3, accidental: Accidental.Flat),
@@ -49,6 +50,38 @@ class ChordRepository {
           NotePosition(note: Note.E, octave: 3),
           NotePosition(note: Note.G, octave: 3),
           NotePosition(note: Note.B, octave: 3),
+        ],
+      ),
+      Chord(
+        name: 'D',
+        notes: [
+          NotePosition(note: Note.D, octave: 3),
+          NotePosition(note: Note.F, octave: 3, accidental: Accidental.Sharp),
+          NotePosition(note: Note.A, octave: 3),
+        ],
+      ),
+      Chord(
+        name: 'Dm',
+        notes: [
+          NotePosition(note: Note.D, octave: 3),
+          NotePosition(note: Note.F, octave: 3),
+          NotePosition(note: Note.A, octave: 3),
+        ],
+      ),
+      Chord(
+        name: 'E',
+        notes: [
+          NotePosition(note: Note.E, octave: 3),
+          NotePosition(note: Note.G, octave: 3, accidental: Accidental.Sharp),
+          NotePosition(note: Note.B, octave: 3),
+        ],
+      ),
+      Chord(
+        name: 'E♭',
+        notes: [
+          NotePosition(note: Note.E, octave: 3, accidental: Accidental.Flat),
+          NotePosition(note: Note.G, octave: 3),
+          NotePosition(note: Note.B, octave: 3, accidental: Accidental.Flat),
         ],
       ),
     ];
